cb29468d06bee21e400092ddf3e386ebfb0d3f94
BaltzarL <106113033+BaltzarL@users.noreply.github.com>
2022-12-01 13:33:33 +0100

    AX

--- main.pl
+++ main.pl
@@ -17,6 +17,13 @@ verify(Input) :-
 
     not_member(Available, Blacklist, A) :- member(A, Available), not(member(A, Blacklist)).
 
+    append([],L,L).
+    append([H|T],L,[H|R]) :- append(T,L,R).
+
+    appendEl(X, [], [X]).
+    appendEl(X, [H | T], [H | Y]) :-
+               appendEl(X, T, Y).
+
 % OPERATIONS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
     % Neg
@@ -30,10 +37,16 @@ verify(Input) :-
     % Or
     check(T, L, S, U, or(F,G)) :- check(T, L, S, U, F) ; check(T, L, S, U, G).
 
-
     % AX
-    % check(T, L, S, U, ax(X)) :- 
-
+    check(T, L, S, U, ax(X)) :- 
+        % Find the available next states
+        member([S, AvailableStates], T), 
+        % Add the current state to RecordedStates
+        appendEl(S, U, RecordedStates)
+        % Generate an available state
+        not_member(AvailableStates, RecordedStates, NewState), 
+        % Check X in the new state
+        check(T, L, NewState, RecordedStates, X).
 
     % EX
     %check(T, L, S, U, ex(X)) :- ...
