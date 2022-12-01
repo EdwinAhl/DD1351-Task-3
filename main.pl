e6a7ca01dcf2b3283b3dab5c4c3b71c8d1d6f401
Edwin <edwin.ahlstrand@bahnhof.se>
2022-12-01 12:46:29 +0100

    added gitignore

--- main.pl
+++ main.pl
@@ -4,7 +4,8 @@
 verify(Input) :-
     see(Input), read(T), read(L), read(S), read(F), seen,
     check(T, L, S, [], F).
-    % check(T, L, S, U, F)
+
+    check(T, L, S, U, F) :- write(T), write("\n"), write(L), write("\n"), write(S), write("\n"), write(U), write("\n"), write(F).
     % T - The transitions in form of adjacency lists
     % L - The labeling
     % S - Current state
