788105668167875c30a83d5d476eacd8f2b53b2b
Edwin <edwin.ahlstrand@bahnhof.se>
2022-12-01 12:59:08 +0100

    structured

--- main.pl
+++ main.pl
@@ -1,31 +1,52 @@
-% For SICStus, uncomment line below: (needed for member/2)
-%:- use_module(library(lists)).
 % Load model, initial state and formula from file.
 verify(Input) :-
-    see(Input), read(T), read(L), read(S), read(F), seen,
+    see(Input), read(T), read(L), read(S), read(F), seen, use_module(library(lists)),
     check(T, L, S, [], F).
 
-    check(T, L, S, U, F) :- write(T), write("\n"), write(L), write("\n"), write(S), write("\n"), write(U), write("\n"), write(F).
     % T - The transitions in form of adjacency lists
     % L - The labeling
     % S - Current state
     % U - Currently recorded states
     % F - CTL Formula to check.
-    %
-    % Should evaluate to true iff the sequent below is valid.
-    %
-    % (T,L), S |- F
-    % U
-    % To execute: consult('your_file.pl'). verify('input.txt').
+
+
+% OPERATIONS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
     % Literals
     %check(_, L, S, [], X) :- ...
+
+
+    % Neg
     %check(_, L, S, [], neg(X)) :- ...
+
+
     % And
     %check(T, L, S, [], and(F,G)) :- ...
+
+
     % Or
+    %check(T, L, S, [], or(F,G)) :- ...
+
+
     % AX
+    %check(T, L, S, [], ax(X)) :- ...
+
+
     % EX
+    %check(T, L, S, [], ex(X)) :- ...
+
+
     % AG
+    %check(T, L, S, [], ag(X)) :- ...
+
+
     % EG
+    %check(T, L, S, [], eg(X)) :- ...
+    
+
     % EF
+    %check(T, L, S, [], ef(X)) :- ...
+
+
     % AF
+    %check(T, L, S, [], af(X)) :- ...
